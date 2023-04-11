fb392d03ced1ff3924ab495d4f2d202cb086071e
Karol Krawczynski <karol.krawczynski4@gmail.com>
2023-04-11 21:32:47 +0200

    Druga wersja generatora

--- v2/post_gen.py
+++ v2/post_gen.py
@@ -1,18 +1,40 @@
 import openai
+import random
 
-openai.api_key = "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"
+# Ustawienie klucza API
+openai.api_key = "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"
 
-model_engine = "text-davinci-002"
-prompt = "Roger Federer won the US Open in 2008."
-completions = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+# Funkcja do generowania postów
+def generate_post(tournament_name, country, surface, results):
+    
+    # Definiowanie promptu
+    prompt = f"Opis turnieju {tournament_name} w {country} na nawierzchni {surface}."
+    
+    # Dodawanie wyników do promptu
+    for i, result in enumerate(results):
+        prompt += f" W meczu {i+1} {result}."
+    
+    # Generowanie odpowiedzi z użyciem ChatGPT-3
+    response = openai.Completion.create(
+        engine="davinci", # Wybranie modelu
+        prompt=prompt, # Prompt
+        temperature=0.7, # Ustawienie losowości
+        max_tokens=150, # Maksymalna liczba tokenów w odpowiedzi
+        n=1, # Ilość odpowiedzi do wygenerowania
+        stop=None, # Zakończenie generowania na pewnym słowie
+    )
+    
+    # Zwrócenie wygenerowanego tekstu
+    return response.choices[0].text.strip()
 
-message = completions.choices[0].text
-print(message)
-print("JUZ")
+# Przykładowe dane
+tournament_name = "Wimbledon"
+country = "Anglia"
+surface = "trawa"
+results = ["Roger Federer pokonał Rafaela Nadala w finale", "Serena Williams zdobyła tytuł w turnieju pańskim"]
+
+# Wygenerowanie postu
+generated_post = generate_post(tournament_name, country, surface, results)
+
+# Wyświetlenie postu
+print(generated_post)
